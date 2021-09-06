42c09704eddb5d9cf53c059ccea6e21a20a12790
Aspect13 <ad13box@gmail.com>
2021-09-06 22:42:38 +0300

    removed static from commit

--- static/js/custom.js
+++
@@ -1,9 +0,0 @@
-const selectedProjectLocalStorageKey = 'selectedProject';
-
-const getSelectedProjectId = () => {
-    let projectId = localStorage.getItem(selectedProjectLocalStorageKey);
-    if (projectId === null) {
-        projectId = getSelectedProjectIdFromBackend().then(id => parseInt(id));
-    }
-    return parseInt(projectId)
-}
