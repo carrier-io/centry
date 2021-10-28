34f40f94eaa6697103800c84722968287391fab5
Aspect13 <ad13box@gmail.com>
2021-10-28 22:02:36 +0300

    fixing test results page

--- static/js/components/security/security.js
+++ static/js/components/security/security.js
@@ -8,38 +8,7 @@ function test_name_button(value, row, index) {
     return `<a class="test form-control-label" href="?${searchParams.toString()}" role="button">${row.name}</a>`
 }
 
-const badgeClasses = {
-    'badge-dark': 0,
-    'badge-light': 0,
-    'badge-info': 0,
-    'badge-warning': 0,
-    'badge-danger': 0,
-    'badge-success': 0,
-    'badge-secondary': 0,
-    'badge-primary': 0,
-}
 
-let tagBadgeMapping = {}
-
-const getTagBadge = tag => {
-    const tagLower = tag.toLowerCase()
-    if (tagBadgeMapping[tagLower] === undefined) {
-        const leastChosenClassName = Object.keys(badgeClasses).reduce(
-            (className, current, index, item) =>
-                badgeClasses[className] < badgeClasses[current] ? className : current
-        )
-        tagBadgeMapping[tagLower] = leastChosenClassName
-        badgeClasses[leastChosenClassName]++
-    }
-    return tagBadgeMapping[tagLower]
-}
-
-function reportsTagFormatter(value, row, index) {
-    const result = value?.map(item => {
-        return `<span class="badge badge-pill ${getTagBadge(item)}">${item}</span>`
-    })
-    return result?.join(' ')
-}
 
 function reportsStatusFormatter(value, row, index) {
     switch (value.toLowerCase()) {
