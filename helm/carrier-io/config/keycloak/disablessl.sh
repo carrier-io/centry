ac28144c4841e6f29b5164e68ac63e0233fd1c15
Attila_Vetesi <Attila_Vetesi@epam.com>
2023-06-08 12:46:14 +0200

    Initial sync of the helm chart with beta

--- helm/carrier-io/config/keycloak/disablessl.sh
+++ helm/carrier-io/config/keycloak/disablessl.sh
@@ -2,3 +2,5 @@
 /opt/jboss/keycloak/bin/kcadm.sh config credentials --server http://localhost/auth --realm master --user carrier
 /opt/jboss/keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE
 /opt/jboss/keycloak/bin/kcadm.sh update realms/carrier -s sslRequired=NONE
+
+# TODO sync with latest version from beta branch
